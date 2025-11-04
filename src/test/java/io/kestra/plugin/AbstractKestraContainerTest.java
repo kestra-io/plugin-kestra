bfd5912d51a302c0b89dde21335fb875da720a82
Roman Acevedo <roman.acevedo62@gmail.com>
2025-11-04 19:26:59 +0100

    test: try to replace tenant init creation

--- src/test/java/io/kestra/plugin/AbstractKestraContainerTest.java
+++ src/test/java/io/kestra/plugin/AbstractKestraContainerTest.java
@@ -1,9 +1,16 @@
 package io.kestra.plugin;
 
-import io.kestra.sdk.KestraClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.kestra.core.http.HttpRequest;
+import io.kestra.core.http.client.HttpClient;
+import io.kestra.core.http.client.configurations.BasicAuthConfiguration;
+import io.kestra.core.http.client.configurations.HttpConfiguration;
+import io.kestra.core.models.property.Property;
 import io.kestra.sdk.internal.ApiException;
 import io.kestra.sdk.model.Tenant;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
@@ -12,7 +19,10 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.net.URI;
 import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @Slf4j
@@ -68,10 +78,18 @@ public class AbstractKestraContainerTest {
         generateData();
     }
 
+    @SneakyThrows
     static void generateData() throws ApiException {
-        KestraClient kc = kestraTestDataUtils.getKestraClient();
-
         Tenant tenant = new Tenant().id(TENANT_ID).name(TENANT_ID);
-//        kc.tenants().create(tenant); TODO make it work again
+
+        var auth  = BasicAuthConfiguration.builder().username(Property.ofValue(USERNAME)).password(Property.ofValue(PASSWORD)).build();
+        var httpClient = HttpClient.builder().configuration(HttpConfiguration.builder().auth(auth).build()).build();
+
+        var res = httpClient.request(HttpRequest.builder()
+                .method("POST")
+                .body(HttpRequest.RequestBody.from(new StringEntity(new ObjectMapper().writeValueAsString(tenant))))
+                .uri(URI.create(KESTRA_URL + "/api/v1/tenants"))
+            .build());
+        assertThat(res.getStatus().getCode()).isEqualTo(201);
     }
 }
