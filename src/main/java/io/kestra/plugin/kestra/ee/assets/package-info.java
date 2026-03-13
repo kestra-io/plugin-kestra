ea984a15dbd1e8f172d1018b04404ac31813074e
brian.mulier <bmmulier@hotmail.fr>
2026-03-13 16:35:35 +0100

    fix(assets): add package-info to display asset plugins

---
+++ src/main/java/io/kestra/plugin/kestra/ee/assets/package-info.java
@@ -0,0 +1,6 @@
+@PluginSubGroup(
+    title = "Kestra Assets", categories = { PluginSubGroup.PluginCategory.CORE }
+)
+package io.kestra.plugin.kestra.ee.assets;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
