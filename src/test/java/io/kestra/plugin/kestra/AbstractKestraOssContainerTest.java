f18bd399793b07262610b2833575599fdfd643e1
Roman Acevedo <roman.acevedo62@gmail.com>
2026-03-31 12:41:45 +0200

    doc: failing tests cause

--- src/test/java/io/kestra/plugin/kestra/AbstractKestraOssContainerTest.java
+++ src/test/java/io/kestra/plugin/kestra/AbstractKestraOssContainerTest.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 public abstract class AbstractKestraOssContainerTest extends AbstractKestraContainerTest {
     @Container
     protected static final GenericContainer<?> KESTRA_OSS_CONTAINER = new AbstractKestraOssContainerTest() {
+        // the issue is here, some test work in 1.3 but not develop
     }.buildContainer("kestra/kestra:develop-no-plugins", false);
 
     @Override
